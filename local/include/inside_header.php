bfe42eab2663681233db96acc86badec0013ef17
sadieff <savintsev.dima@gmail.com>
2020-03-25 15:10:41 +0300

    Add header and footer files for inside pages.

--- local/include/inside_header.php
+++ local/include/inside_header.php
@@ -1,1 +1,6 @@
-1
+<div class="news-detail">
+    <div class="container">
+        <div class="news-detail_title">
+            <? $APPLICATION->ShowTitle(); ?>
+        </div>
+        <div class="news-detail_content content_">
